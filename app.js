4fb7265cfaa9ef5efb0b374b34f8258cc0e23c80
EmmN <emanuel.nedelcu@gmail.com>
2016-03-17 09:53:51 +0200

    use correct port

--- app.js
+++ app.js
@@ -31,5 +31,5 @@ app.get('/image', function(req, res) {
   });  
 });
 
-app.listen(process.env.PORT);
-console.log('listening on port '+ process.env.PORT);
+app.listen(process.env.PORT || 3000);
+console.log('listening on port '+ (process.env.PORT || 3000));
